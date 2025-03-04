c1d86ac5abe151021dccf4765bb65e53ef78642a
Nana Yaw <jeffrey-14@live.co.uk>
2025-03-04 17:07:36 -0500

    Initial Commit

---
+++ MyCalculatorApp/MyCalculatorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCalculatorAppApp.swift
+//  MyCalculatorApp
+//
+//  Created by Nana Yaw on 11/7/24.
+//
+
+import SwiftUI
+
+@main
+struct MyCalculatorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyCalculatorAppTests/MyCalculatorAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyCalculatorAppTests.swift
+//  MyCalculatorAppTests
+//
+//  Created by Nana Yaw on 11/7/24.
+//
+
+import Testing
+@testable import MyCalculatorApp
+
+struct MyCalculatorAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
